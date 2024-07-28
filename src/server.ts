813d797516ee9aaf7f97dac24f106702ab13ad06
GremaBR <gremabr@gmail.com>
2024-07-28 01:42:56 -0300

    addTransactionsRoute

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,19 @@
 import fastify from 'fastify'
 import crypto from 'node:crypto'   
 import { knex } from './database'
+import { env } from './env'
+import { transactionRoutes } from './routes/transactions'
 
 
 const app = fastify()
 
-
-app.get('/hello', async () =>{
-    const transaction = await knex('transactions')
-    .where('amount', 1000)
-    .select('*')
-
-    return transaction
+app.register(transactionRoutes, {
+    prefix:'transactions',
 })
 
 app
 .listen({
-    port: 3333,
+    port: env.PORT,
 }).then(() =>{
     console.log('HTTP Server Running!')
 })
